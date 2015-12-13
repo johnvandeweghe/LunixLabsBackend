3a5376ff5ebe8f52ac28e76c99ec4f1fc1a2022f
John VanDeWeghe <vandeweghe@lunixlabs.com>
2015-12-13 03:26:57 -0600

    Update projects data model, add some entries

--- src/Entities/Projects.php
+++ src/Entities/Projects.php
@@ -11,7 +11,22 @@ class Projects
     /** @Column(type="string") */
     private $name;
 
+    /** @Column(type="string") */
+    private $url;
+
+    /** @Column(type="string", name="short_description") */
+    private $shortDescription;
+
+    /** @Column(type="string") */
+    private $description;
+
     public function toArray(){
-        return ['id' => $this->id, "name" => $this->name];
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'url' => $this->url,
+            "short_description" => $this->shortDescription,
+            "description" => $this->description
+        ];
     }
 }
